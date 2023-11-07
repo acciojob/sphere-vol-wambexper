786474d8013c8006128e86e162173243ca6a1bae
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-07 17:16:10 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    // Get the value of the radius input field
+    var radius = parseFloat(document.getElementById('Radius').value);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    // Check if the input is a valid number
+    if (!isNaN(radius)) {
+        // Calculate the volume of the sphere using the formula
+        var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+
+        // Update the Volume field with the calculated volume
+        document.getElementById('Volume').value = volume.toFixed(2);
+    } else {
+        // If the input is not a valid number, display an error message
+        document.getElementById('Volume').value = "Invalid input";
+    }
+
+    // Prevent the form from submitting
+    return false;
+}
+
+// Attach the volume_sphere function to the form's submit event
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
